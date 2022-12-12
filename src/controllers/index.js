c1e2ac425834f255f425811eb450a7c2cc3eb443
Pratham Garg <73795688+Prathamgarg17@users.noreply.github.com>
2022-12-12 15:33:37 +0530

    To do list

---
+++ src/controllers/index.js
@@ -0,0 +1,68 @@
+const { response } = require("express");
+const { Student } = require("../models/models");
+
+var students = ["S1", "S2", "S3", "S4", "New Student"];
+const getStudentsOld = (request, response) => {
+  var id = request.query.id;
+  //TodO: mETHOD - 1
+  // if (id) {
+  //   return response.send(students[id]);
+  // } else {
+  //   return response.send(students);
+  // }
+
+  // tOdo: Method 2
+  return response.json({
+    data:
+      id === undefined
+        ? students
+        : students[id] === undefined
+        ? "No Data found for this ID"
+        : students[id],
+  });
+};
+
+const getStudents = async (request, response) => {
+  var studentId = request.query.id;
+  if (studentId) {
+    try {
+      var allStudents = await Student.findById(studentId);
+    } catch {
+      allStudents = null;
+    }
+  } else {
+    var allStudents = await Student.find();
+  }
+
+  return response.json(allStudents);
+};
+
+const createNewStudent = async (request, response) => {
+  await Student.create(request.body);
+  return response.json({ data: "Student Created" });
+};
+
+const updateStudent = async (request, response) => {
+    var studentId = request.query.id;
+    try {
+      var student = await Student.findById(studentId);
+      if (!student) {
+        return response
+          .status(404)
+          .json({ status: "Error", msg: "Id does not exist" });
+      }
+    } catch {
+      return response
+        .status(404)
+        .json({ status: "Error", msg: "Id does not exist" });
+    }
+  
+    await Student.findByIdAndUpdate(studentId, request.body);
+    return response.json({ data: "Student Updated" });
+  };
+const deleteStudent = async (request, response) => {
+    var studentId = request.query.id;
+    await Student.findByIdAndDelete(studentId);
+    return response.json({ data: "Student Deleted" });
+  };
+module.exports = { createNewStudent, getStudents,updateStudent,deleteStudent};
