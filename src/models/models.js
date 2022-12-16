a17a55aa9aaecc0d756642b1d3b5996ade615888
Pratham Garg <73795688+Prathamgarg17@users.noreply.github.com>
2022-12-16 14:43:38 +0530

    Split Name

--- src/models/models.js
+++ src/models/models.js
@@ -1,10 +1,28 @@
 const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema({
-  name: String,
-  email: String,
+  // name:{
+  //  type: String,
+  //  trim: true,    
+  // }, 
+  email:{
+    type: String,
+    unique: true
+  },
   age: Number,
+  first_name: String,
+  last_name: String,
 });
+studentSchema
+.virtual("name")
+.set(function (name){
+  var splitedName = name.split(" ");
+  this.first_name= splitedName[0];
+  this.last_name= splitedName[1];
+})
+.get(function(){
+  return this.first_name+" "+this.last_name;
+})
 
 const Student = mongoose.model("Student", studentSchema);
 module.exports = { Student };
